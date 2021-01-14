5e84b3d1ca2312354cb45bcd9783be1435a864d6
Carceles Mickael <mickael.carceles.etu@univ-lille.fr>
2021-01-14 11:10:52 +0100

    ADD : BufferedWriter

--- TP1/FTPServer.java
+++ TP1/FTPServer.java
@@ -13,18 +13,27 @@ public class FTPServer {
         int port = 8080;
         //InetAddress address = "127.0.0.1";
         ServerSocket s = new ServerSocket(port);
-        InputStreamReader reader;
         System.out.println("Server Socket Ready");
         Socket c = new Socket();
+
+        BufferedReader br;
+        BufferedWriter wr;
+        String message = "";
+
         try
         {
             while(serverOn)
             {
                 c = s.accept();
                 System.out.println("Client connected");
-                reader = new InputStreamReader(c.getInputStream());
+                br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                wr = new BufferedWriter(new OutputStreamWriter(c.getOutputStream()));
                 
-
+                message = "220 Service ready for new user.\r\n";
+                wr.write(message);
+                wr.newLine();
+                wr.close();
+                
             }
         }
         catch(Exception ex)
