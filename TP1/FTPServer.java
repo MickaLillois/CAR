b1b2b06dc917a01e95d00f3f22e9f075bca65bc2
Carceles Mickael <mickael.carceles.etu@univ-lille.fr>
2021-01-21 09:32:04 +0100

    ADD : architecture en thread

--- TP1/FTPServer.java
+++ TP1/FTPServer.java
@@ -1,95 +1,37 @@
 import java.io.*;
 import java.lang.*;
 import java.net.*;
+import java.util.Date;
+import java.sql.Timestamp;
+
 
 public class FTPServer {
-
-    public static String login = "user";
-    public static String passwd = "pwd";
-    public static boolean serverOn = true;
-
-
-    public static final String CODE_215 = "215 Remote system type is UNIX.\r\n";
-    public static final String CODE_220 = "220 Service ready for new user.\r\n";
-    public static final String CODE_221 = "221 Service closing control connection.\r\n";
-    public static final String CODE_230 = "230 User logged in.\r\n"; 
-    public static final String CODE_331 = "331 User name okay, need password.\r\n";
-    public static final String CODE_530 = "530 Not logged in.\r\n";
 
     public static void main(String[] args) throws Exception
     {
         int port = 8080;
         ServerSocket s = new ServerSocket(port);
-        System.out.println("Server Socket Ready");
-        Socket c = new Socket();
-
-        BufferedReader br;
-        BufferedWriter wr;
-        String message = "";
-        String response = "";
+        Date date = new Date();
+        System.out.println("[" + new Timestamp(date.getTime()) + "] INFO -" + " Server ON");
+       
+        Socket c = new Socket(); 
+        int id = 1;
 
         try
         {
-            while(serverOn)
+            while(true)
             {
                 c = s.accept();
-                
-                System.out.println("Client connected");
-
-                br = new BufferedReader(new InputStreamReader(c.getInputStream()));
-                wr = new BufferedWriter(new OutputStreamWriter(c.getOutputStream()));
-                
-                write(wr,CODE_220);
-                response = br.readLine();
-                //permet de récupérer juste le username (voir @FTPRequestAnnotation)
-                response = (response.split(" ")[1]);
-
-                if(response.equals(login))
-                {
-                    write(wr,CODE_331);
-                    response = br.readLine();
-                    response = (response.split(" ")[1]);
-                    if(response.equals(passwd))
-                    {    
-                        write(wr,CODE_230);     
-                        write(wr,CODE_215);   
-                        
-                        while(serverOn)
-                        {
-                            response = br.readLine();
-                            switch(response.split(" ")[0])
-                            {
-                                case "QUIT":
-                                    write(wr,CODE_221);
-                                    serverOn = false;
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        write(wr,CODE_530);
-                        serverOn = false;
-                    }
-                }
-                else
-                {
-                    write(wr,CODE_530);
-                    serverOn = false;
-                }
-
+                Runnable rThread = new MyRunnableThread(c,id);
+                Thread newThread = new Thread(rThread);
+                newThread.start();
+                id += 1;
             }
         }
         catch(Exception ex)
         {
-            System.out.println("An error occured.");
-        }
-        finally{
-            c.close();
-            System.out.println("Client disconnected");
-            s.close();
+            System.err.println("An error occured : ");
+			ex.printStackTrace();
         }
     }
 
@@ -103,5 +45,99 @@ public class FTPServer {
     {
         wr.write(message);
         wr.flush();
+    }
+}
+
+class MyRunnableThread implements Runnable 
+{
+    Socket c = new Socket();
+    int id = 0;
+
+
+    public static String login = "user";
+    public static String passwd = "pwd";
+    public static final String CODE_215 = "215 Remote system type is UNIX.\r\n";
+    public static final String CODE_220 = "220 Service ready for new user.\r\n";
+    public static final String CODE_221 = "221 Service closing control connection.\r\n";
+    public static final String CODE_230 = "230 User logged in.\r\n"; 
+    public static final String CODE_331 = "331 User name okay, need password.\r\n";
+    public static final String CODE_530 = "530 Not logged in.\r\n";
+
+
+    BufferedReader br;
+    BufferedWriter wr;
+    String message = "";
+    String response = "";
+
+    public MyRunnableThread(Socket newSocket,int id) 
+    {
+        this.c = newSocket;
+        this.id = id;
+    }
+
+    public void run() 
+    {   
+        Date date = new Date();
+        try
+        {   
+            System.out.println("[" + new Timestamp(date.getTime()) + "] INFO -" + " Client " + id + " connected");
+
+            br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+            wr = new BufferedWriter(new OutputStreamWriter(c.getOutputStream()));
+
+            FTPServer.write(wr,CODE_220);
+            response = br.readLine();
+            //permet de récupérer juste le username (voir @FTPRequestAnnotation)
+            response = (response.split(" ")[1]);
+
+            if(response.equals(login))
+            {
+                FTPServer.write(wr,CODE_331);
+                response = br.readLine();
+                response = (response.split(" ")[1]);
+                if(response.equals(passwd))
+                {    
+                    FTPServer.write(wr,CODE_230);     
+                    FTPServer.write(wr,CODE_215);   
+
+                    System.out.println("[" + new Timestamp(date.getTime()) + "] INFO -" + " Client " + id + " logged in");
+                    
+                    while(!c.isClosed())
+                    {
+                        response = br.readLine();
+                        switch(response.split(" ")[0])
+                        {
+                            case "QUIT":
+                                FTPServer.write(wr,CODE_221);
+                                c.close();
+
+                                System.out.println("[" + new Timestamp(date.getTime()) + "] INFO -" + " Client " + id + " quitted");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    FTPServer.write(wr,CODE_530);
+                    c.close();
+
+                    System.out.println("[" + new Timestamp(date.getTime()) + "] INFO -" + " Client " + id + " disconnected : bad password");
+                }
+            }
+            else
+            {
+                FTPServer.write(wr,CODE_530);
+                c.close();
+
+                System.out.println("[" + new Timestamp(date.getTime()) + "] INFO -" + " Client " + id + " disconnected : bad loggin");
+            }
+        }
+        catch(Exception ex)
+        {
+            System.err.println("[" + new Timestamp(date.getTime()) + "] ERROR -" + " An exception occured");
+            ex.printStackTrace();
+        }
     }
 }
