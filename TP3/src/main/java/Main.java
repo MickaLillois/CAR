43e5eba98770a9482343ddfec9aa3db3f5c49777
Carceles Mickael <mickael.carceles.etu@univ-lille.fr>
2021-03-18 12:08:39 +0100

    Session du 18/03

--- TP3/src/main/java/Main.java
+++ TP3/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import actors.Master;
+import actors.Mapper;
+import actors.Reducer;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ActorSystem system = ActorSystem.create("MySystem");
+        ActorRef master;
+
+
+        master = system.actorOf(Props.create(Master.class), "master");
+
+
+        system.shutdown();
     }
 }
