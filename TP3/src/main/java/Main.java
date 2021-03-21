8c259d23ca568240b1b8989c260b353bb9ddd9e6
Carceles Mickael <mickael.carceles.etu@univ-lille.fr>
2021-03-21 14:37:10 +0100

    UPDATE : Avancée en autonomie

--- TP3/src/main/java/Main.java
+++ TP3/src/main/java/Main.java
@@ -5,14 +5,24 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("MySystem");
         ActorRef master;
 
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 
         master = system.actorOf(Props.create(Master.class), "master");
 
+        system.actorOf(Props.create(Mapper.class), "mapper1");
+        system.actorOf(Props.create(Mapper.class), "mapper2");
+
+        master.tell( "le message",ActorRef.noSender());
+        master.tell( new Object(),ActorRef.noSender());
+
+        master.tell(new File(classloader.getResource("le_corbeau_et_le_renard.txt").getFile()), ActorRef.noSender());
 
         system.shutdown();
     }
--- TP3/src/main/java/actors/Master.java
+++ TP3/src/main/java/actors/Master.java
@@ -1,4 +1,45 @@
 package actors;
 
-public class Master {
+import akka.actor.UntypedActor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Master extends UntypedActor { //job : distribuer les lignes
+
+    public File fileToProcess;
+
+    public void onReceive( Object message ) throws InterruptedException {
+        if(message instanceof String)
+        {
+            System.out.println("string");
+        }
+        else if (message instanceof File)
+        {
+            fileToProcess = (File) message;
+            this.sendLines();
+        }
+        else
+        {
+            System.out.println("unhandled");
+            unhandled(message);
+        }
+    }
+
+    private void sendLines()
+    {
+        int i = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileToProcess))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                //mapper1.tell(line.toUpperCase(), getSelf());
+                i++;
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
